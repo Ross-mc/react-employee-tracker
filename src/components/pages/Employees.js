84222afdb53ba4dd8ea4ac52b0836d581644288e
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-03-24 22:33:34 +0000

    filter func refactoring

--- src/components/pages/Employees.js
+++ src/components/pages/Employees.js
@@ -1,65 +1,79 @@
 import React from "react";
 import API from "../../utils/API";
-import EmployeeTable from "../EmployeeTable"
-import SearchForm from "../SearchForm"
+import EmployeeTable from "../EmployeeTable";
+import SearchForm from "../SearchForm";
 
-class Employees extends React.Component{
-
+class Employees extends React.Component {
   state = {
     employees: [],
     displayedEmployees: [],
     category: "",
-    searchTerm: ""
-  }
+    searchTerm: "",
+  };
 
-  componentDidMount(){
+  componentDidMount() {
     this.getEmployees();
   }
 
   getEmployees = () => {
-    API.getEmployees().then(res => {
+    API.getEmployees().then((res) => {
       console.log(res.data.results);
-      this.setState({employees: res.data.results, displayedEmployees: res.data.results})
-    })
-  }
+      this.setState({
+        employees: res.data.results,
+        displayedEmployees: res.data.results,
+      });
+    });
+  };
 
-  onChangeHandler = event => {
+  onChangeHandler = (event) => {
     const value = event.target.value;
     const name = event.target.name;
     const stateObject = this.state;
     stateObject[name] = value;
-    this.setState({stateObject})
-  }
+    this.setState({ stateObject });
+  };
 
   filterEmployees = (column, value) => {
-    
+    const lowerCaseValue = value.toLowerCase().trim();
+    let filteredEmployees = [];
+    //name comes from first and last property of name object, so we use template to get the full value.
+    if (column === "name") {
+      filteredEmployees = this.state.employees.filter(({ name }) => {
+        const fullName = `${name.first} ${name.last}`.toLowerCase();
+        return fullName.startsWith(lowerCaseValue);
+      });
+    } else {
+      //city and country are both on location object, use [] syntax for dynamic call to country or city
+      filteredEmployees = this.state.employees.filter(({ location }) =>
+        location[column].toLowerCase().startsWith(lowerCaseValue)
+      );
+    }
 
-    const filteredEmployees = this.state.employees.filter(employee => {
-      if (column === 'country'){
-        return employee.location.country === value
-      }
+    this.setState({ displayedEmployees: filteredEmployees });
+  };
 
-      return employee[column] === value
-    });
-    this.setState({displayedEmployees: filteredEmployees})
-  }
-
-  searchHandler = event => {
+  searchHandler = (event) => {
     event.preventDefault();
-    this.filterEmployees(this.state.category, this.state.searchTerm)
+    this.filterEmployees(this.state.category, this.state.searchTerm);
+  };
 
-  }
-
-  render(){
-    return(
+  render() {
+    return (
       <div>
         <h1>Search all Dinoco Employees</h1>
-        <SearchForm searchHandler={this.searchHandler} onChangeHandler={this.onChangeHandler}/>
+        <SearchForm
+          searchHandler={this.searchHandler}
+          onChangeHandler={this.onChangeHandler}
+        />
         <div>
-          {this.state.displayedEmployees.length === 0 ? "No Employees Found. This may be a problem with our API or your search parameters" : <EmployeeTable employees={this.state.displayedEmployees}/>}
+          {this.state.displayedEmployees.length === 0 ? (
+            "No Employees Found. This may be a problem with our API or your search parameters"
+          ) : (
+            <EmployeeTable employees={this.state.displayedEmployees} />
+          )}
         </div>
-      </div>     
-    )
+      </div>
+    );
   }
 }
 

d79f623a5d2b0a0aebdff3ebb07db97de2c1cfcc
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-03-25 08:32:44 +0000

    restructured form element using bootstrap classes

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const SearchForm = ({searchHandler, onChangeHandler}) => {
+const SearchForm = ({ searchHandler, onChangeHandler }) => {
   return (
-    <form onSubmit={searchHandler}>
-      <div className="mb-3">
+    <form onSubmit={searchHandler} style={{ width: "60%", margin: "0 auto" }}>
+      <div className="form-group">
         <label for="search-term" className="form-label">
           Search term
         </label>
@@ -15,50 +15,56 @@ const SearchForm = ({searchHandler, onChangeHandler}) => {
           name="searchTerm"
           onChange={onChangeHandler}
         />
-        <div id="search-category-aria" className="form-text">
+        <div id="search-category-aria" className="form-text text-center">
           Pick your category below
         </div>
       </div>
-      <div className="form-check form-check-inline">
-        <input
-          className="form-check-input"
-          type="radio"
-          name="category"
-          id="name"
-          value="name"
-          onChange={onChangeHandler}
-        />
-        <label className="form-check-label" for="name">
-          Name
-        </label>
+      <div className="form-group text-center">
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="radio"
+            name="category"
+            id="name"
+            value="name"
+            onChange={onChangeHandler}
+          />
+          <label className="form-check-label" for="name">
+            Name
+          </label>
+        </div>
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="radio"
+            name="category"
+            id="city"
+            value="city"
+            onChange={onChangeHandler}
+          />
+          <label className="form-check-label" for="city">
+            City
+          </label>
+        </div>
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="radio"
+            name="category"
+            id="country"
+            value="country"
+            onChange={onChangeHandler}
+          />
+          <label className="form-check-label" for="country">
+            Country
+          </label>
+        </div>
       </div>
-      <div className="form-check form-check-inline">
-        <input
-          className="form-check-input"
-          type="radio"
-          name="category"
-          id="city"
-          value="city"
-          onChange={onChangeHandler}
-        />
-        <label className="form-check-label" for="city">
-          City
-        </label>
+      <div className="form-group text-center">
+        <button type="submit" className="btn btn-primary">
+          Submit Search
+        </button>
       </div>
-      <div className="form-check form-check-inline">
-        <input
-          className="form-check-input"
-          type="radio"
-          name="category"
-          id="country"
-          value="country"
-          onChange={onChangeHandler}
-        />
-        <label className="form-check-label" for="country">
-          Country
-        </label>
-      </div>
-      <button type="submit" className="btn btn-primary">Submit Search</button>
     </form>
   );
 };

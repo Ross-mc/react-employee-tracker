eda291364509485d45b27bdad4f5063228c45f41
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-03-26 08:45:48 +0000

    Added FA and arrow component

---
+++ src/components/Arrow.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Arrow = (direction = "straight", arrowClickHandler) => {
+    return (
+        <i className={direction === "straight" ? "fas fa-grip-lines" : `fas fa-angle-${direction}`} onClick={arrowClickHandler}></i>
+    )
+}
+
+export default Arrow

5f6ec76de2736863217aafc50759b4cbca115b53
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-03-26 08:51:56 +0000

    testing arrows

--- src/components/Arrow.js
+++ src/components/Arrow.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Arrow = (direction = "straight", arrowClickHandler) => {
+const Arrow = ({direction, arrowClickHandler}) => {
     return (
         <i className={direction === "straight" ? "fas fa-grip-lines" : `fas fa-angle-${direction}`} onClick={arrowClickHandler}></i>
     )
--- src/components/EmployeeTable.js
+++ src/components/EmployeeTable.js
@@ -1,12 +1,13 @@
 import React from "react";
 import EmployeeRow from "./EmployeeRow"
+import Arrow from "./Arrow"
 
 const EmployeeTable = ({employees}) => {
   return (
     <table className="table">
       <thead>
         <tr>
-          <th scope="col">ID</th>
+          <th scope="col">ID <Arrow direction={'straight'} arrowClickHandler={() => console.log('arrow clicked')}/></th>
           <th scope="col">Name</th>
           <th scope="col">Phone</th>
           <th scope="col">Email</th>
